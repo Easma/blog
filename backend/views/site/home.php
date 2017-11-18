b0fc6cc22b4d549a5d029a8ae7f2b14b826ddcd6
easma <easma@foxmail.com>
2017-11-18 17:57:03 +0800

    dev

--- backend/views/site/home.php
+++ backend/views/site/home.php
@@ -1,6 +1,8 @@
 <?php
+/* @var $this yii\web\View */
 
 \backend\assets\BackendAsset::addCss($this,'@web/css/overwrite.adminlet.css');
+$baseUrl = Yii::$app->request->baseUrl;
 ?>
 
 <!-- row -->
@@ -129,30 +131,7 @@
             </li>
             <!-- END timeline item -->
             <!-- timeline item -->
-            <li>
-                <i class="fa fa-video-camera bg-maroon"></i>
 
-                <div class="timeline-item">
-                    <span class="time"><i class="fa fa-clock-o"></i> 5 days ago</span>
-
-                    <h3 class="timeline-header"><a href="#">Mr. Doe</a> shared a video</h3>
-
-                    <div class="timeline-body">
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <video id="my-video" class="video-js" controls preload="auto" poster="http://vjs.zencdn.net/v/oceans.png" >
-                                <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4">
-                                <p class="vjs-no-js">
-                                    To view this video please enable JavaScript, and consider upgrading to a web browser that
-                                    <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-                                </p>
-                            </video>
-                        </div>
-                    </div>
-                    <div class="timeline-footer">
-                        <a href="#" class="btn btn-xs bg-maroon">See comments</a>
-                    </div>
-                </div>
-            </li>
             <!-- END timeline item -->
 
             <!-- timeline time label -->
@@ -201,114 +180,152 @@
             </li>
             <!-- END timeline item -->
 
-            <!-- timeline time label -->
+
             <li class="time-label">
-                  <span class="bg-orange">
-                      2013
-                  </span>
+                  <span class="bg-orange">2013</span>
             </li>
-            <!-- /.timeline-label -->
-            <!-- timeline item -->
             <li>
                 <i class="fa fa-camera bg-purple"></i>
-
                 <div class="timeline-item">
-                    <span class="time"><i class="fa fa-clock-o"></i> 3月6日11点</span>
-
-                    <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-
-                    <div class="timeline-body">
-
-                    </div>
-                </div>
-            </li>
-            <!-- END timeline item -->
-
-
-            <!-- timeline time label -->
-            <li class="time-label">
-                  <span class="bg-olive">
-                      2012
-                  </span>
-            </li>
-            <!-- /.timeline-label -->
-            <!-- timeline item -->
-            <li>
-                <i class="fa fa-camera bg-purple"></i>
-
-                <div class="timeline-item">
-                    <span class="time"><i class="fa fa-clock-o"></i> 3月6日11点</span>
-
-                    <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-
-                    <div class="timeline-body">
-
-                    </div>
-                </div>
-            </li>
-            <!-- END timeline item -->
-
-            <!-- timeline time label -->
-            <li class="time-label">
-                  <span class="bg-blue">
-                      2011
-                  </span>
-            </li>
-            <!-- /.timeline-label -->
-            <!-- timeline item -->
-            <li>
-                <i class="fa fa-camera bg-purple"></i>
-
-                <div class="timeline-item">
-                    <span class="time"><i class="fa fa-clock-o"></i> 3月6日11点</span>
-
-                    <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-
-                    <div class="timeline-body">
-
-                    </div>
-                </div>
-            </li>
-            <!-- END timeline item -->
-
-            <!-- timeline time label -->
-            <li class="time-label">
-                  <span class="bg-maroon">
-                      2010
-                  </span>
-            </li>
-            <!-- /.timeline-label -->
-            <!-- timeline item -->
-            <li>
-                <i class="fa fa-camera bg-purple"></i>
-
-                <div class="timeline-item">
-                    <span class="time"><i class="fa fa-clock-o"></i> 3月6日11点</span>
-
+                    <span class="time"><i class="fa fa-clock-o"></i> 5月1日13点</span>
                     <h3 class="timeline-header">
-                        <a><i class="fa fa-map-marker"></i> 湖南·张家界</a>
+                        <a><i class="fa fa-map-marker"></i> 怀化</a>
                     </h3>
-
                     <div class="timeline-body">
                         <div class="timeline-title">
-                            阿达的说法的发生的发生的发的说法的是发的发的发是打发打发打发是的但是发送到sdfsdf
+                            像素渣就是好
                         </div>
-                        <a data-fancybox="gallery2" href="http://via.placeholder.com/700x560" title="">
-                            <img src="http://placehold.it/66x80" alt="..." class="margin">
-                        </a>
-                        <a data-fancybox="gallery2" href="http://via.placeholder.com/700x560" title="">
-                            <img src="http://placehold.it/66x80" alt="..." class="margin">
-                        </a>
-                        <a data-fancybox="gallery2" href="http://via.placeholder.com/700x560" title="">
-                            <img src="http://placehold.it/66x80" alt="..." class="margin">
-                        </a>
-                        <a data-fancybox="gallery2" href="http://via.placeholder.com/700x560" title="">
-                            <img src="http://placehold.it/66x80" alt="..." class="margin">
-                        </a>
+                        <?php for ($i=1;$i<=2;$i++) { ?>
+                            <a data-fancybox="gallery009" href="<?=Yii::$app->request->baseUrl?>/img/2013/20130102_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" title="">
+                                <img width="100" height="80" src="<?=Yii::$app->request->baseUrl?>/img/2013/S20130102_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" alt="..." class="margin">
+                            </a>
+                        <?php } ?>
                     </div>
                 </div>
             </li>
-            <!-- END timeline item -->
+
+
+
+
+            <li class="time-label">
+                  <span class="bg-olive">2012</span>
+            </li>
+            <li>
+                <i class="fa fa-video-camera bg-maroon"></i>
+
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 12月12日15点</span>
+                    <h3 class="timeline-header">
+                        <a><i class="fa fa-map-marker"></i> 怀化学院</a>
+                    </h3>
+                    <div class="timeline-body">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <video id="my-video" class="video-js" controls preload="auto" poster="<?=$baseUrl?>/img/2012/20121212_v1.jpg" >
+                                <source src="<?=$baseUrl?>/img/2012/20121212.mp4" type="video/mp4">
+                                <p class="vjs-no-js">
+                                    To view this video please enable JavaScript, and consider upgrading to a web browser that
+                                    <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                                </p>
+                            </video>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+
+
+            <li class="time-label">
+                  <span class="bg-blue">2011</span>
+            </li>
+            <li>
+                <i class="fa fa-camera bg-purple"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 10月5日16点</span>
+                    <h3 class="timeline-header">
+                        <a><i class="fa fa-map-marker"></i> 湘西·德夯</a>
+                    </h3>
+                    <div class="timeline-body">
+                        <?php for ($i=1;$i<=6;$i++) { ?>
+                            <a data-fancybox="gallery003" href="<?=$baseUrl?>/img/2011/20111005_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" title="">
+                                <img width="100" height="80" src="<?=$baseUrl?>/img/2011/S20111005_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" alt="..." class="margin">
+                            </a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <i class="fa fa-camera bg-purple"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 5月13日14点</span>
+                    <h3 class="timeline-header">
+                        <a><i class="fa fa-map-marker"></i> 浙江·杭州</a>
+                    </h3>
+                    <div class="timeline-body">
+                        <?php for ($i=1;$i<=10;$i++) { ?>
+                            <a data-fancybox="gallery003" href="<?=$baseUrl?>/img/2011/20110513_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" title="">
+                                <img width="100" height="80" src="<?=$baseUrl?>/img/2011/S20110513_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" alt="..." class="margin">
+                            </a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <i class="fa fa-camera bg-purple"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 3月25日20点</span>
+                    <h3 class="timeline-header">
+                        <a><i class="fa fa-map-marker"></i> 湘西·凤凰文化古城</a>
+                    </h3>
+                    <div class="timeline-body">
+                        <?php for ($i=1;$i<=3;$i++) { ?>
+                            <a data-fancybox="gallery003" href="<?=$baseUrl?>/img/2011/20110325_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" title="">
+                                <img width="100" height="80" src="<?=$baseUrl?>/img/2011/S20110325_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" alt="..." class="margin">
+                            </a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </li>
+
+
+
+            <li class="time-label">
+                  <span class="bg-maroon">2010</span>
+            </li>
+            <li>
+                <i class="fa fa-camera bg-purple"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 10月5日10点</span>
+                    <h3 class="timeline-header">
+                        <a><i class="fa fa-map-marker"></i> 怀化·芷江</a>
+                    </h3>
+                    <div class="timeline-body">
+                        <?php for ($i=1;$i<=9;$i++) { ?>
+                        <a data-fancybox="gallery002" href="<?=$baseUrl?>/img/2010/20101005_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" title="">
+                            <img width="100" height="80" src="<?=$baseUrl?>/img/2010/S20101005_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" alt="..." class="margin">
+                        </a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <i class="fa fa-camera bg-purple"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 5月1日13点</span>
+                    <h3 class="timeline-header">
+                        <a><i class="fa fa-map-marker"></i> 怀化</a>
+                    </h3>
+                    <div class="timeline-body">
+                        <div class="timeline-title">
+                            一起骑自行车闲逛
+                        </div>
+                        <?php for ($i=1;$i<=3;$i++) { ?>
+                            <a data-fancybox="gallery001" href="<?=$baseUrl?>/img/2010/20100501_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" title="">
+                                <img width="100" height="80" src="<?=$baseUrl?>/img/2010/S20100501_<?= str_pad($i, 3, "0", STR_PAD_LEFT);?>.jpg" alt="..." class="margin">
+                            </a>
+                        <?php } ?>
+                    </div>
+                </div>
+            </li>
 
             <li>
                 <i class="fa fa-clock-o bg-gray"></i>
